b4686a9e2a1807573982641abcacc203defe1003
chinthub <chintan.98.mehta@gmail.com>
2021-04-10 21:33:43 +0530

    Added files

---
+++ AddTwoNumbers.py
@@ -0,0 +1,93 @@
+'''
+Problem Statement #2
+You are given two non-empty linked lists representing two non-negative integers 
+The digits are stored in reverse order, and each of their nodes contains a single digit 
+Add the two numbers and return the sum as a linked list
+You may assume the two numbers do not contain any leading zero, except the number 0 itself
+
+Example 1:
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807
+
+Example 2:
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Example 3:
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+'''
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+#Definition of solution pseudocode
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        list1, list2 = [], []
+        temp = l1
+        count1 = count2 = 0
+        while(temp != None):
+            count1 += 1
+            list1.append(temp.val)
+            temp = temp.next
+        temp = l2
+        while(temp != None):
+            count2 += 1
+            list2.append(temp.val)
+            temp = temp.next
+        num1 = num2 = 0
+        n = 1
+        for i in (list1):
+            num1 = num1 + (i * n)
+            n = n * 10
+        n = 1
+        for i in (list2):
+            num2 = num2 + (i * n)
+            n = n * 10
+        sum1 = num1 + num2
+        res = [int(x) for x in str(sum1)]
+        curr = dummy = ListNode(0)
+        for i in reversed(res):
+            curr.next = ListNode(i)
+            curr = curr.next
+        return dummy.next
+
+
+"""
+Efficient Solution : 
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        
+        #   :type l1: ListNode
+        #   :type l2: ListNode
+        #   :rtype: ListNode
+        
+        result = ListNode(0)
+        result_tail = result
+        carry = 0
+                
+        while l1 or l2 or carry:            
+            val1  = (l1.val if l1 else 0)
+            val2  = (l2.val if l2 else 0)
+            carry, out = divmod(val1 + val2 + carry, 10)    
+                      
+            result_tail.next = ListNode(out)
+            result_tail = result_tail.next                      
+            
+            l1 = (l1.next if l1 else None)
+            l2 = (l2.next if l2 else None)
+               
+        return result.next
+
+"""
